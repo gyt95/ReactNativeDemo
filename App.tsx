42ff2052b1649dd076d21a8ecc3785c6cdc02824
kwan <gyt95@163.com>
2023-02-08 23:44:06 +0800

    complete the environment construction

--- App.tsx
+++ App.tsx
@@ -40,7 +40,7 @@ function Section({children, title}: SectionProps): JSX.Element {
             color: isDarkMode ? Colors.white : Colors.black,
           },
         ]}>
-        {title}
+        {title}我终于成功啦！
       </Text>
       <Text
         style={[
